895616cba2b1f129eaef1579d36929e76066eb1d
Fathimathansi <fathimathansi68@gmail.com>
2025-11-05 00:41:06 +0530

    Update Orderlist.jsx

--- client/src/Pages/Seller/Orderlist.jsx
+++ client/src/Pages/Seller/Orderlist.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import { useNavigate,Link } from 'react-router-dom';
-import './OrderList.css';
+import './Orderlist.css';
 
 const OrderList = () => {
   const [orders, setOrders] = useState([]);
